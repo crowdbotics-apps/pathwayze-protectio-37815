7a1fbe4c50f8edfd7c69a2cdaa6541676c90d24e
Crowdbotics <team@crowdbotics.com>
2022-12-06 19:57:30 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pathwayze_protectio_37815/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pathwayze_protectio_37815;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "pathwayze_protectio_37815";
+  }
+}
