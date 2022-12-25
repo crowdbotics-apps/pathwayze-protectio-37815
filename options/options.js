48ed33eb3b024cde9a18b80576a05e4be3b3e8b3
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2022-12-25 20:55:08 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "pathwayze_protectio_37815",
   url: "https://pathwayze_protectio_37815.botics.co",
   api: "https://pathwayze_protectio_37815.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
